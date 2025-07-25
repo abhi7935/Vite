4db25498dcfb0aaaefd974b8933918887a3ab50b
abhishek <abhishekkhare102@gmail.com>
2025-07-26 01:03:00 +0530

    text updated

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React (Abhishek Khare)</h1>
+      <p>Ye hai jalwa</p>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
